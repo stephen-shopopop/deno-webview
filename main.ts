9d3ba98bbf796c1fbea19008eab243499cb706c8
stephen-shopopop <94382341+stephen-shopopop@users.noreply.github.com>
2022-11-08 11:05:52 +0100

    feat(launch): ADD Goo env deno

--- main.ts
+++ main.ts
@@ -2,8 +2,14 @@ import { Webview } from "webview";
 
 const html = `
   <html>
+  <style>
+  body {
+    background: gray
+  }
+  </style>
   <body>
     <h1>Hello from deno v${Deno.version.deno}</h1>
+    <footer>I am hungry</footer>
   </body>
   </html>
 `;
@@ -11,4 +17,5 @@ const html = `
 const webview = new Webview();
 
 webview.navigate(`data:text/html,${encodeURIComponent(html)}`);
+webview.title = "Broz";
 webview.run();
